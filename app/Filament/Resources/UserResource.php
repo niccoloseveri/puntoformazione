46925ca8b8a90b59c7f5c1b52530e040e5d2c31e
niccoloseveri <niccolo.severi@libero.it>
2024-02-13 10:35:57 +0100

    removed unused references

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,23 +3,15 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
-use Filament\Forms\Get;
 use Filament\Forms\Set;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Resources\Resource;
 use Filament\Support\RawJs;
 use Filament\Tables;
-use Filament\Tables\Columns\Column;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\Log;
 
 class UserResource extends Resource
 {
@@ -41,16 +33,16 @@ class UserResource extends Resource
                     ->mask(RawJs::make(<<<'JS'
                         $input.replace(/\w\S*/g, function(txt) {
                             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                        });
-                    JS))
+                            });
+                        JS))
                     ->afterStateUpdated(fn (Set $set, ?string $state) => $set('password', str_replace(' ','',$state).'0000')),
                 Forms\Components\TextInput::make('surname')->label('Cognome')
                     ->required()
                     ->mask(RawJs::make(<<<'JS'
                         $input.replace(/\w\S*/g, function(txt) {
                             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                        });
-                    JS))
+                            });
+                        JS))
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
                     ->email()
@@ -65,62 +57,57 @@ class UserResource extends Resource
                         'data-lpignore' => 'true',
                         'data-1p-ignore'
                     ])
-                    //->disabled()
                     ->maxLength(255)
                     ->helperText('La password è formata dal nome dello studente + 0000')
                     ->password()
                     ->revealable()
                     ->dehydrated(fn ($state) => filled($state))
-                    ->required(fn ($livewire) => ($livewire instanceof CreateRecord))
-                    ,
+                    ->required(fn ($livewire) => ($livewire instanceof CreateRecord)),
                 Forms\Components\TextInput::make('cf')->label('Codice Fiscale')
                     ->required()
                     ->maxLength(16)
                     ->mask(RawJs::make(<<<'JS'
-                    $input.toUpperCase();
-                JS))->regex("/^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i")
-                ->validationMessages([
-                    'regex' => 'Codice Fiscale non valido.'
-                ]),
+                        $input.toUpperCase();
+                        JS))
+                    ->regex("/^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i")
+                    ->validationMessages([
+                        'regex' => 'Codice Fiscale non valido.'
+                    ]),
                 Forms\Components\TextInput::make('via')
                     ->required()
                     ->mask(RawJs::make(<<<'JS'
                         $input.replace(/\w\S*/g, function(txt) {
                             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                        });
-                    JS))
+                            });
+                        JS))
                     ->maxLength(255),
                 Forms\Components\TextInput::make('citta')
                     ->required()
                     ->mask(RawJs::make(<<<'JS'
                         $input.replace(/\w\S*/g, function(txt) {
                             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                        });
-                    JS))
+                            });
+                        JS))
                     ->maxLength(255),
                 Forms\Components\TextInput::make('cap')
                     ->required()
                     ->mask(RawJs::make(<<<'JS'
                         $input.replace(/\w\S*/g, function(txt) {
                             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                        });
-                    JS))
+                            });
+                        JS))
                     ->maxLength(255),
                 Forms\Components\TextInput::make('tel')
                     ->tel()
                     ->required()
                     ->maxLength(255),
-
-                Select::make('roles')
-                /*->color('gray')*/
-                ->multiple()
-                ->relationship('roles', 'name')
-                ->preload()
-                ->label('Ruolo'),
-
+                Forms\Components\Select::make('roles')
+                    ->multiple()
+                    ->relationship('roles', 'name')
+                    ->preload()
+                    ->label('Ruolo'),
                 Forms\Components\Toggle::make('is_teacher')->label('Insegnante?')
                     ->required(),
-
             ]);
     }
 
@@ -129,38 +116,36 @@ class UserResource extends Resource
         return $table
             ->defaultPaginationPageOption(25)
             ->columns([
-                Tables\Columns\TextColumn::make('full_name')->label('Nome e Cognome')->copyable()
+                Tables\Columns\TextColumn::make('full_name')
+                    ->label('Nome e Cognome')
+                    ->copyable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email')->copyable()
+                Tables\Columns\TextColumn::make('email')
+                    ->copyable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('cf')->label('Codice Fiscale')->copyable()
+                Tables\Columns\TextColumn::make('cf')
+                    ->label('Codice Fiscale')
+                    ->copyable()
                     ->searchable(),
-                Tables\Columns\TextColumn::make('address')->label('Indirizzo')
+                Tables\Columns\TextColumn::make('address')
+                    ->label('Indirizzo')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('tel')
+                    ->label('Telefono')
+                    ->copyable()
                     ->searchable(),
-                Tables\Columns\IconColumn::make('roles.id')->getStateUsing(function ($record) {
-                    if($record->roles->first()->id == 1) $a = 1;
-                    else $a=0;
-                    return $a;
-                })->boolean()->alignCenter()->label('Insegnante'),
-                Tables\Columns\IconColumn::make('is_teacher')
-                    ->boolean()->toggleable(isToggledHiddenByDefault:true),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\IconColumn::make('roles.id')
+                    ->getStateUsing(function ($record) {
+                        if($record->roles->first()->id == 1) $a = 1;
+                        else $a=0;
+                        return $a;
+                    })
+                    ->boolean()
+                    ->alignCenter()
+                    ->label('Insegnante'),
             ])
             ->filters([
-                //TEST
+                //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
